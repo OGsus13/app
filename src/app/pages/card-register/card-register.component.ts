c5f8d14ef6ec7497c716aa0394e38fde61405e4d
Chuy <brandondgh666@gmail.com>
2022-07-07 15:39:20 -0500

    MIS CAMBIOS EN API MARVEL RANFLAS

--- src/app/pages/card-register/card-register.component.ts
+++ src/app/pages/card-register/card-register.component.ts
@@ -20,25 +20,25 @@ datacard: cardmodel[]= [
     cardtitul :"TITULO 1",
     descri: "descripcion",
     boton: "1",
-    il: "https://images8.alphacoders.com/104/1042296.jpg"
+    il: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjegBJyGExptm83UtjLQcYJN_lM7T-ayFrVA&usqp=CAU"
   },
   {
     cardtitul :"TITULO2",
     descri: "descripcion2",
     boton: "2",
-    il: "https://images6.alphacoders.com/470/470300.jpg"
+    il: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjegBJyGExptm83UtjLQcYJN_lM7T-ayFrVA&usqp=CAU"
   },
   {
     cardtitul :"TITULO3",
     descri: "descripcion3",
     boton: "3",
-    il: "https://images5.alphacoders.com/518/518518.jpg"
+    il: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjegBJyGExptm83UtjLQcYJN_lM7T-ayFrVA&usqp=CAU"
   },
   {
     cardtitul :"TITULO4",
     descri: "descripcion4",
     boton: "4",
-    il: "https://images2.alphacoders.com/902/902988.jpg"
+    il: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHzZHX5QtAkblBE-f8qcCy7Da_6SCJ1smTNFE4cyviwBTrq2vpHnN-U00MypyHS7uSZvA&usqp=CAU"
   }
 ]
 
